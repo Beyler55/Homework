88ee71982bc69bfec03c2b5ff9835e0be7ff7450
BeylerSadigov <104448603+Beyler55@users.noreply.github.com>
2022-05-15 14:06:10 +0400

    Pyramida with method

---
+++ Pyramida.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Param
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintPyramida(5);
+        }
+
+        static void PrintPyramida(int width)
+        {
+            for (int i = 1; i <= width; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
