e5cb5cdf90c8884c95af04a51427badc63f7021e
BeylerSadigov <104448603+Beyler55@users.noreply.github.com>
2022-05-15 14:07:19 +0400

    array with parameters

---
+++ Param.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Param
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfElem(1, 2, 3, 4));
+        }
+
+        static bool SumOfElem(params int[] arr)
+        {
+            bool value = false;
+            int sum = 0;
+            foreach (int elem in arr)
+            {
+                sum += elem;
+            }
+
+            if (sum % 2 == 0)
+            {
+                value = true;
+                return value;
+            }
+            return value;
+        }
+
+    }
+}
