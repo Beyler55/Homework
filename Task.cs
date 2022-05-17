ebdd7ceea7881811f5b47d145789ff4cdcbe28df
BeylerSadigov <104448603+Beyler55@users.noreply.github.com>
2022-05-17 15:48:43 +0400

    Add files via upload

---
+++ Task.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RevString());
+        }
+        static string RevString()
+        {
+            string names = "Nurlan Faiq Emil Tural Yusif";
+            string[] spl = names.Split(' ');
+            string revname = "";
+            string temp = "";
+            foreach (var item in spl)
+            {
+
+                for (int i = item.Length - 1; i >= 0; i--)
+                {
+                    revname = revname + item[i];
+
+                }
+
+            }
+            return revname;
+        }
+    }
+}
