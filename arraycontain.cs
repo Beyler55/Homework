e0e129206f6c17eb1cc97901acc68624148665af
BeylerSadigov <104448603+Beyler55@users.noreply.github.com>
2022-05-18 18:14:36 +0400

    IsArrayContains method task

---
+++ arraycontain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstarr = { 1, 2, 3, 4, 5, 6 };
+            int[] secondarr = { 3, 5 };
+            Console.WriteLine(IsArrayContains(firstarr,secondarr));
+        }
+
+        static bool IsArrayContains(int[] firstarr,int[] secondarr)
+        {
+            string str1 = string.Join(' ', firstarr);
+            string str2 = string.Join(' ', secondarr);
+            return str1.Contains(str2);        
+        }
+    }
+}
