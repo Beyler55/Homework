b1f402a2f2b6d4b4fc70b5444a49edc3205a5f59
BeylerSadigov <104448603+Beyler55@users.noreply.github.com>
2022-05-22 22:39:39 +0400

    added task

---
+++ charraystr.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace chararraystr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] array = { "Akif", "Rahim", "Sirac", "alma" };
+            CharInStringArray(array, 'm');
+           
+        }
+        static void CharInStringArray(string[]arr,char ch)
+        {
+            foreach (var item in arr)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (item[i] == ch)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+        }
+    }
+}
