02ec8ff922defbdf897a09e036c6fa42c3f5040a
BeylerSadigov <104448603+Beyler55@users.noreply.github.com>
2022-05-24 16:45:05 +0400

    constructors

--- Program.cs
+++ Program.cs
@@ -1,32 +1,12 @@
-﻿using System;
-
-namespace arraysorttask
+﻿namespace constructor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] array = { 5, 1, 3, 2, 4, 8 };
-            CustomSort(array);
-        }
-        static void CustomSort(int[] arr)
-        {
-            for (int i = 1; i < arr.Length; i++)
-            {
-                for (int j = 0; j < arr.Length; j++)
-                {
-                    int temp = arr[j];
-                    if (arr[i] < arr[j])
-                    {
-                        arr[j] = arr[i];
-                        arr[i] = temp;
-                    }
-                }
-            }
-            foreach (var item in arr)
-            {
-                Console.WriteLine(item);
-            }
-        }
+            Person person = new Person("Arif","Hasanov",19){};
+            
+            System.Console.WriteLine(person.IsAdult);
     }
+}
 }
