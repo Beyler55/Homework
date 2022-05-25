078a4cb8c0af5042b29204096158961845d517f2
BeylerSadigov <104448603+Beyler55@users.noreply.github.com>
2022-05-25 16:28:32 +0400

    user student grade

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
-﻿namespace constructor
+﻿using System;
+namespace inheritance
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person("Arif","Hasanov",19){};
+            User user1 = new Student("Vusal", "Musayev", "vusaloo", "vusal2002",80);
+            User user2 = new Student("Anar","Guliyev","anarguli","guliyev22",70 );
+            User user3 = new Student("Ulvi","Mammadov","mammadov233","mulvi5535",75);
+
+            User[] users = { user1, user2, user3 };
+            Console.WriteLine("Please enter the username:");
+            string getName = Console.ReadLine();
+            Console.WriteLine("Please enter the password:");
+            string getPass = Console.ReadLine();
             
-            System.Console.WriteLine(person.IsAdult);
+            foreach (var item in users)
+            {
+                if(item.Username==getName && item.Password==getPass)
+                {
+                    item.GetData();
+                    
+                }
+            }
+        }
     }
-}
 }
---
+++ Student.cs
@@ -0,0 +1,20 @@
+﻿namespace inheritance
+{
+    class Student : User
+    {
+
+        public Student(string name,string surname,string username, string password,  int grade )
+        {
+            Username = username;
+            Surname = surname;
+            Password = password;
+            Grade = grade;
+            Name = name;
+        }
+        public int Grade;
+        public override void GetData()
+        {
+            System.Console.WriteLine(Grade);
+        }
+    }
+}
